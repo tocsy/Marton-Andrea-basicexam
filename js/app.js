6bbb781f900404cc7a1fdf1599ef3e8aa9c1cf60
Andrea Marton <andi.marton@gmail.com>
2018-06-11 10:52:24 +0200

    files uploaded to repo

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function getData(url, callbackFunc) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      callbackFunc(this);
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+function successAjax(xhttp) {
+  // Innen lesz elérhető a JSON file tartalma, tehát az adatok amikkel dolgoznod kell
+  var userDatas = JSON.parse(xhttp.responseText);
+  
+}
+
+getData('/json/spaceships.json', successAjax);
